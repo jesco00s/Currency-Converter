d759693227400f03ae2936a622752a959810edaa
j0e009t <jose.escobar1@walmart.com>
2018-02-07 22:32:06 -0600

    - Can now convert USD to Yuan (China)

--- app/src/main/java/com/escobar/j0e009t/currencyconverter/MainActivity.java
+++ app/src/main/java/com/escobar/j0e009t/currencyconverter/MainActivity.java
@@ -16,14 +16,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void convertPesos(View view){
-        EditText usdEditText = findViewById(R.id.USD_Amount);
-        String usdString = usdEditText.getText().toString();
-        Double usdAmount = Double.parseDouble(usdString);
-
+        Double usdAmount = getUSDAmount(R.id.USD_Amount);
         Double pesosAmount = (usdAmount) * 18.76;
         String pesosString = pesosAmount.toString();
 
         TextView textView = findViewById(R.id.convertedResponse);
         textView.setText("Pesos: $" + pesosString);
+    }
+
+    public void convertYuan(View view){
+        Double usdAmount = getUSDAmount(R.id.USD_Amount);
+        Double yuanAmount = (usdAmount) * 6.32;
+        String yenString = yuanAmount.toString();
+
+        TextView textView = findViewById(R.id.convertedResponse);
+        textView.setText("Yuan: $" + yenString);
+    }
+
+    private double getUSDAmount(int id){
+        EditText usdEditText = findViewById(id);
+        String usdString = usdEditText.getText().toString();
+        Double usdAmount = Double.parseDouble(usdString);
+        return usdAmount;
     }
 }
